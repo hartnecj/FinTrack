62de9a4cd9889c3248641439d3be5e3cd38ebbd3
Mary R <147426551+MaryRei@users.noreply.github.com>
2026-02-19 12:22:51 -0600

    Update register.php 02/19/2026
    fixed the routing of the navigation, fixed css routing and javascript routing, added comments to the code and updated it on hostinger.
    
    -Mary

--- auth/register.php
+++ auth/register.php
@@ -41,71 +41,87 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 ?>
 <!DOCTYPE html>
 <html lang="en">
+  <!-- head tag -->
   <head>
+    <!-- title -->
     <title>FinTrack- SignUp</title>
+    <!-- link to bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- link to JQuery -->
     <script src="https://code.jquery.com/jquery-4.0.0.js" integrity="sha256-9fsHeVnKBvqh3FB2HYu7g2xseAZ5MlN6Kz/qnkASV8U=" crossorigin="anonymous"></script>
+    <!-- meta viewport tag -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/assets/style.css">
-  </head>
+    <!-- link to stylesheet -->
+    <link rel="stylesheet" href="../assets/landingStyles.css">
+  </head><!-- end head tag -->
+  <!-- start body tag -->
   <body>
-    <nav>
+      
+    <nav><!-- navigation section -->
       <ul>
-        <li><a href="/landing.php"><button class="btn">Home</button></a></li>
-        <li><a href="/auth/login.php"><button class="btn">Login</button></a></li>
-        <li><a href="/auth/register.php"><button class="btn">Sign Up</button></a></li>
+        <li><a href="../landing.php"><button class="btn page-btn">Home</button></a></li>
+        <li><a href="/auth/login.php"><button class="btn page-btn">Login</button></a></li>
+        <li><a href="#"><button class="btn page-btn">Register</button></a></li>
       </ul>
-    </nav>
+    </nav><!-- end navigation-->
 
+    <!-- main section -->
     <section class="main-container shadow-lg">
-      <form method="post" action="/auth/register.php" autocomplete="on">
+      <!-- start form section -->
+      <form method="POST" action="">
         <fieldset class="form-group">
           <legend>Sign Up</legend>
-
+          
           <?php if ($error !== ''): ?>
             <div class="alert alert-danger" role="alert"><?php echo htmlspecialchars($error); ?></div>
           <?php endif; ?>
-
-          <label for="username">Desired Username</label><br>
-          <div class="mb-3 input-group">
-            <input type="text" id="username" name="username" class="form-control" required>
-          </div>
-
-          <label for="password">Password</label><br>
-          <div class="mb-3 input-group">
-            <input type="password" id="password" name="password" class="form-control" required>
-          </div>
-
-          <label for="passwordConfirm">Password Confirm</label><br>
-          <div class="mb-3 input-group">
-            <input type="password" id="passwordConfirm" name="passwordConfirm" class="form-control" required>
-          </div>
-        </fieldset>
-
-        <fieldset class="form-group">
+          
+          <!-- email input -->
           <label for="email">E-Mail</label><br>
           <div class="mb-3 input-group">
-            <input type="email" id="email" name="email" class="form-control" required>
+            <input type="email" id="email" name="email" class="form-control" aria-label="Email input" aria-description="email-input box for entering email" required>
           </div>
-
-          <label for="first-name">First Name</label><label for="last-name">Last Name</label>
+          
+          <!-- password input -->
+          <label for="password">Password</label><br>
           <div class="mb-3 input-group">
-            <input type="text" id="first-name" name="first-name" class="form-control" required>
-            <input type="text" id="last-name" name="last-name" class="form-control" required>
+            <input type="password" id="password" name="password" class="form-control" aria-label="Password input" aria-description="password-input box for entering password"required>
+          </div>
+          
+          <!-- password confirm input -->
+          <label for="passwordConfirm">Password Confirm</label><br>
+          <div class="mb-3 input-group">
+            <input type="password" id="passwordConfirm" name="passwordConfirm" class="form-control" aria-label="Password input confirmation" aria-description="password-input box for entering password to check if they match" required>
           </div>
         </fieldset>
-
-        <input class="btn" type="submit" value="Sign Up" id="login-btn">
+        
+        <fieldset class="form-group">
+          
+          <!-- name input -->
+          <label for="first-name" class="input-label">First Name</label><label for="last-name" class="input-label left-input-label">Last Name</label>
+          <div class="mb-3 input-group">
+            <!-- first name input -->
+            <input type="text" id="first-name" name="first-name" class="form-control" aria-label="First-Name input" aria-description="first-name input box" required>
+            <!-- last name input -->
+            <input type="text" id="last-name" name="last-name" class="form-control" aria-label="Last-Name input" aria-description="last-name input box" required>
+          </div>
+          
+        </fieldset>
+        <!-- submit button -->
+        <input class="btn" type="submit" value="Sign up" id="signup-btn">
       </form>
-    </section>
-
+    </section><!-- end main section -->
+    
+    <!-- footer section -->
     <footer>
       <div class="form-check form-switch">
         <input class="form-check-input" type="checkbox" role="switch" id="styleSwitch">
         <label class="form-check-label" for="styleSwitch" id="styleLabel"> Light mode: On </label>
       </div>
-    </footer>
+    </footer><!-- end footer -->
 
-    <script src="/assets/pageCustomization.js"></script>
-  </body>
-</html>
+    <!-- link to JS -->
+    <script src="../assets/pageCustomization.js">     
+    </script>
+  </body><!-- end body tag -->
+</html><!-- end html tag -->
