958b5bae1591f698810fb18f3a6702c23901b8d6
Mary R <147426551+MaryRei@users.noreply.github.com>
2026-04-24 11:17:20 -0500

    Update edit_expenses.php
    4/24/26 added aria labels for accessability

--- edit_expenses.php
+++ edit_expenses.php
@@ -326,17 +326,17 @@ $expenses = $stmt->fetchAll(PDO::FETCH_ASSOC);
         <div class="row">
           <div class="col-md-6 mb-3">
             <label class="form-label">Amount</label>
-            <input type="number" step="0.01" min="0.01" class="form-control" name="amount" value="<?= htmlspecialchars($expense['amount'] ?? ''); ?>" required>
+            <input type="number" step="0.01" min="0.01" class="form-control" name="amount" value="<?= htmlspecialchars($expense['amount'] ?? ''); ?>" aria-label="expense amount" required>
           </div>
 
           <div class="col-md-6 mb-3">
-            <label class="form-label">Date</label>
-            <input type="date" class="form-control" name="expense_date" required value="<?= htmlspecialchars($expense['expense_date'] ?? ''); ?>">
+            <label class="form-label">Expense Date</label>
+            <input type="date" class="form-control" name="expense_date" required value="<?= htmlspecialchars($expense['expense_date'] ?? ''); ?>" aria-label="expense date">
           </div>
 
           <div class="col-md-12 mb-3">
             <label class="form-label">Budget (optional)</label>
-            <select class="form-control" name="budget_id">
+            <select class="form-control" name="budget_id" aria-label="optional budget select" >
               <option value="0">No budget</option>
               <?php foreach ($budgets as $b): ?>
                 <option value="<?php echo (int)$b['id']; ?>">
@@ -348,18 +348,18 @@ $expenses = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
           <div class="col-md-8 mb-3">
                 <label class="form-label">Category (optional)</label>
-                <input type="text" class="form-control" name="category" placeholder="Groceries">
+                <input type="text" class="form-control" name="category" placeholder="Groceries" aria-label="expense category" >
             </div>
                 
             <div class="col-md-4 mb-3">
                 <label class="form-label">Personal? (optional)</label><br>
-                <input type="checkbox" class="form-check-input" name="personal" value="1">
+                <input type="checkbox" class="form-check-input" name="personal" value="1" aria-label="personal?" >
             </div>
 
 
           <div class="col-md-12 mb-3">
             <label class="form-label">Description (optional)</label>
-            <input type="text" class="form-control" name="description" value="<?= htmlspecialchars($expense['description'] ?? ''); ?>">
+            <input type="text" class="form-control" name="description" value="<?= htmlspecialchars($expense['description'] ?? ''); ?>" aria-label="expense description" >
           </div>
         </div>
      
